b177262beba6a5f118846cd14dd9d34dabb06762
Xinyu Zhou <zxytim@gmail.com>
2019-05-06 14:00:05 +0800

    add the basic example

---
+++ examples/00-basic/main.py
@@ -0,0 +1,12 @@
+from libhpman.m import _
+
+# forward static parsing
+_.parse_file(__file__)
+print(_.get_value('learning_rate'))
+
+# define hyper-parameters
+learning_rate = _('learning_rate', 1e-3)
+
+# override default value
+_.set_value('learning_rate', 1e-2)
+print(_.get_value('learning_rate'))
