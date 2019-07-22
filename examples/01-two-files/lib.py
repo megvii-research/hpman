93217ab4b7307d85d17060d4145d0219856d81fe
Xinyu Zhou <zxy@megvii.com>
2019-07-22 11:12:03 +0800

    a better example

---
+++ examples/01-two-files/lib.py
@@ -0,0 +1,9 @@
+from libhpman.m import _
+
+
+def add():
+    return _("a", 0) + _("b", 0)
+
+
+def mult():
+    return _("a") * _("b")
---
+++ examples/01-two-files/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import argparse
+
+from libhpman.m import _
+
+import lib
+
+
+def main():
+    basedir = os.path.dirname(os.path.realpath(__file__))
+    _.parse_file(basedir)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", default=_.get_value("a"), type=int)
+    parser.add_argument("-b", default=_.get_value("b"), type=int)
+    args = parser.parse_args()
+
+    _.set_value("a", args.a)
+    _.set_value("b", args.b)
+
+    print("a = {}".format(_.get_value("a")))
+    print("b = {}".format(_.get_value("b")))
+    print("lib.add() = {}".format(lib.add()))
+    print("lib.mult() = {}".format(lib.mult()))
+
+
+if __name__ == "__main__":
+    main()
