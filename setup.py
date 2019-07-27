3afa511c1b81308e260b20520829dbf2d061cb0a
Xinyu Zhou <zxy@megvii.com>
2019-07-27 16:18:25 +0800

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hpman",
-    version="0.0.1",
+    version="0.0.2",
     author="EMTF",
     author_email="emtf@megvii.com",
     description="A hyperparameter manager for deep learning experiments and more",
