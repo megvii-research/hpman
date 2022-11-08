c915dcc8c41d4c65a507e9d3af9f67d4b118e1e7
Xinyu Zhou <zxytim@gmail.com>
2022-11-08 12:49:11 +0800

    bump version

--- hpman/__version__.py
+++ hpman/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "hpman"
-__version__ = "0.11.0"
+__version__ = "0.12.0"
 __author__ = "Xinyu Zhou"
 __author_email__ = "zxy@megvii.com"
 __url__ = "https://github.com/megvii-research/hpman"
